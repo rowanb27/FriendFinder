5f4d42899f7a0ccce5baea3aaaa6fc204da11aa7
Robert Rowan <rowanb27@yahoo.com>
2019-05-18 22:31:54 -0500

    Complete list of friends object 1/10

---
+++ app/data/friends.js
@@ -0,0 +1,75 @@
+//Friends Data
+
+var friendsData = [
+    
+    {
+  "name":"Ahmed",
+  "photo":"https://previews.123rf.com/images/ocskaymark/ocskaymark1708/ocskaymark170800233/84528125-portrait-of-an-elderly-woman-with-alzheimer-disease-listening-to-music-with-headphones-in-the-park-o.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a bubbly personality"
+  },
+
+   {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  },
+
+  {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  },
+
+  {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  },
+
+  {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  },
+
+  {
+  "name":"Ahmed",
+  "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpreviews.123rf.com%2Fimages%2Focskaymark%2Focskaymark1708%2Focskaymark170800233%2F84528125-portrait-of-an-elderly-woman-with-alzheimer-disease-listening-to-music-with-headphones-in-the-park-o.jpg&imgrefurl=https%3A%2F%2Fwww.123rf.com%2Fphoto_84528125_portrait-of-an-elderly-woman-with-alzheimer-disease-listening-to-music-with-headphones-in-the-park-o.html&docid=aqX4WBXvCYGPkM&tbnid=buaqpj7c1mIt_M%3A&vet=10ahUKEwiHkZfX0KbiAhUKX60KHZ7nA28QMwhsKAIwAg..i&w=1300&h=866&bih=722&biw=1600&q=elderly%20woman&ved=0ahUKEwiHkZfX0KbiAhUKX60KHZ7nA28QMwhsKAIwAg&iact=mrc&uact=8",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  },
+
+  {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  },
+
+  {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  },
+
+  {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  },
+
+  {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
+  "short-bio": "People tell me I have a unique personality"
+  }
+
+];
