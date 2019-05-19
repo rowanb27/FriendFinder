254494e7a12baf594fee5e02823781be710562dc
Robert Rowan <rowanb27@yahoo.com>
2019-05-18 23:45:02 -0500

    Added listener

--- server.js
+++ server.js
@@ -13,4 +13,20 @@ var htmlRoutes = require('./FriendFinder/app/routing/html-routes.js');
 
 // Set up Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 7777;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// Server Routing Map 
+apiRoutes(app); // API route - Must be listed first due to the HTML default catch all "use" route
+htmlRoutes(app); // HTML route 
+
+
+// Listener - Start the server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
