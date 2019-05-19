178b4cb6f698272c776ef5e91caaa7406593bad4
Robert Rowan <rowanb27@yahoo.com>
2019-05-18 23:24:23 -0500

    Added GET and POST routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,23 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
+console.log('API Route Connected Successfully');
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+// Links Friends data
+var friendsData = require('../data/friends.js');
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+   //Function includes a Get and Post route
+   function apiRoutes(app) {
+
+   // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends
+   app.get("/api/friends", function(req, res) {
+       res.json(friendsData);
+   });
+   // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+   app.post("api/friends", function(req, res) {
+
+       // Parse new friend input to get integers (AJAX post seemed to make the numbers strings)
+    var newFriend = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: [],
+        quote: req.body.quote
+   })
+   }
