b45c940f542bc84c10a597ea01b07229cc6c7782
Robert Rowan <rowanb27@yahoo.com>
2019-05-18 23:06:32 -0500

    List of friends complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,73 +3,73 @@
 var friendsData = [
     
     {
-  "name":"Ahmed",
+  "name":"Margaret",
   "photo":"https://previews.123rf.com/images/ocskaymark/ocskaymark1708/ocskaymark170800233/84528125-portrait-of-an-elderly-woman-with-alzheimer-disease-listening-to-music-with-headphones-in-the-park-o.jpg",
   "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
   "short-bio": "People tell me I have a bubbly personality"
   },
 
    {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Piper",
+  "photo":"https://i.pinimg.com/originals/0e/0a/f5/0e0af57295f463b300ccdb5908fc189e.jpg",
+  "scores":[1, 1, 3, 2, 2, 3, 5, 5, 3, 2],
+  "short-bio": "Secret to a long and healthy life? Weekly cigar"
   },
 
   {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Mary",
+  "photo":"https://images.theconversation.com/files/101198/original/image-20151109-16242-1kdxrx5.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=496&fit=clip",
+  "scores":[5, 3, 4, 5, 4, 4, 4, 3, 3, 2],
+  "short-bio": "I'm the center of attention at parties"
   },
 
   {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Wilma",
+  "photo":"https://thoughtcatalog.files.wordpress.com/2018/08/witch-movies.jpg?w=1920&h=1280&crop=1&resize=1920,1280&quality=95&strip=all",
+  "scores":[1, 2, 1, 1, 5, 5, 5, 4, 4, 4],
+  "short-bio": "Don't be shy. I won't bite."
   },
 
   {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Angelina",
+  "photo":"http://static.oprah.com/images/tows/201009/20100902-tows-dr.-diller-six-steps-7-300x205.jpg",
+  "scores":[1, 2, 3, 4, 5, 5, 4, 3, 2, 1],
+  "short-bio": "Just here to sell my mirror collection. Call me if interested: 816-555-3776"
   },
 
   {
-  "name":"Ahmed",
-  "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpreviews.123rf.com%2Fimages%2Focskaymark%2Focskaymark1708%2Focskaymark170800233%2F84528125-portrait-of-an-elderly-woman-with-alzheimer-disease-listening-to-music-with-headphones-in-the-park-o.jpg&imgrefurl=https%3A%2F%2Fwww.123rf.com%2Fphoto_84528125_portrait-of-an-elderly-woman-with-alzheimer-disease-listening-to-music-with-headphones-in-the-park-o.html&docid=aqX4WBXvCYGPkM&tbnid=buaqpj7c1mIt_M%3A&vet=10ahUKEwiHkZfX0KbiAhUKX60KHZ7nA28QMwhsKAIwAg..i&w=1300&h=866&bih=722&biw=1600&q=elderly%20woman&ved=0ahUKEwiHkZfX0KbiAhUKX60KHZ7nA28QMwhsKAIwAg&iact=mrc&uact=8",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Hillary",
+  "photo":"https://www.trbimg.com/img-5b352fd9/turbine/bs-ed-older-women-hate-20170111",
+  "scores":[2, 2, 3, 4, 4, 1, 1, 1, 1, 3],
+  "short-bio": "I failed to reach my life long dream. Looking for a rebound"
   },
 
   {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Nikki",
+  "photo":"https://www.rd.com/wp-content/uploads/2017/08/03_hair_Everyday-Habits-that-Make-You-Look-Younger_437756866_Nina-Buday-380x254.jpg",
+  "scores":[5, 3, 4, 3, 3, 3, 4, 3, 3, 2],
+  "short-bio": "There's more to life than work"
   },
 
   {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Brittany",
+  "photo":"http://photos1.blogger.com/x/blogger2/7134/1383/320/674781/helen_thomas.jpg",
+  "scores":[5, 4, 3, 2, 2, 5, 3, 3, 2, 1],
+  "short-bio": "Good heart over cute face anyday"
   },
 
   {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Lou Anne",
+  "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlDZVHCuyE0CReyDQGU94KWZFXpwVb4Eq1tTI_ez6s8WgFrGFu",
+  "scores":[5, 1, 4, 4, 2, 4, 2, 3, 5, 1],
+  "short-bio": "I collect towels"
   },
 
   {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5, 3, 3, 3, 3, 3, 3, 3, 3, 2],
-  "short-bio": "People tell me I have a unique personality"
+  "name":"Mindy",
+  "photo":"https://media.istockphoto.com/photos/senior-woman-with-obcene-gesture-picture-id157672627?k=6&m=157672627&s=612x612&w=0&h=aF_kddPoU83zkIiPukYw24WgOlXiyCtdUhuPyTX9Gg4=",
+  "scores":[5, 3, 3, 3, 2, 5, 5, 5, 5, 2],
+  "short-bio": "Wanting at least three children"
   }
 
 ];
