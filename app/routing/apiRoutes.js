a6716c8ca40790431a2a2a25897d631142bb579c
Robert Rowan <rowanb27@yahoo.com>
2019-05-18 23:30:17 -0500

    Add for loop to check new friend entry with existing one

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,5 +19,23 @@ var friendsData = require('../data/friends.js');
         photo: req.body.photo,
         scores: [],
         quote: req.body.quote
-   })
-   }
+    };
+    var scoresArray = [];
+    for (var i = 0; i < req.body.scores.length; i++){
+        scoresArray.push( parseInt(req.body.scores[i]) )
+    }
+    newFriend.scores = scoresArray;
+
+    //Cross check the new friend entry with the existing ones
+    var scoreComparisionArray = [];
+    for(var i=0; i < friendsData.length; i++){
+
+      // Check each friend's scores and sum difference in points
+      var currentComparison = 0;
+      for(var j=0; j < newFriend.scores.length; j++){
+        currentComparison += Math.abs( newFriend.scores[j] - friendsData[i].scores[j] );
+      }
+
+      // Push each comparison between friends to array
+      scoreComparisionArray.push(currentComparison);
+    }
